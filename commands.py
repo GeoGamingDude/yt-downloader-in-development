1f69e254d86c14575da7282848cb1285cb208a18
GeoGamingDude <geogames73@gmail.com>
2022-10-26 13:02:11 -0400

    changed function

--- commands.py
+++ commands.py
@@ -3,7 +3,7 @@ import os
 
 def get_video(yt:str):
     yt = YouTube(yt)
-    yt = yt.streams.filter(type="video")
+    yt = yt.streams.filter(file_extension="mp4")
     yt.get_highest_resolution().download()
 
 def get_audio(yt:str):
