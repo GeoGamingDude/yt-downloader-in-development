a608f88281052fbbcc24c88e5cbf941d86e5ea5b
GeoGamingDude <geogames73@gmail.com>
2022-10-26 14:02:18 -0400

    fixed video/audio downloads

--- commands.py
+++ commands.py
@@ -3,10 +3,10 @@ import os
 
 def get_video(yt:str):
     yt = YouTube(yt)
-    yt = yt.streams.filter(file_extension="mp4")
-    yt.get_highest_resolution().download()
+    yt = yt.streams.filter(only_video=True)
+    yt.first().download("./Downloads/Videos")
 
 def get_audio(yt:str):
     yt = YouTube(yt)
-    yt = yt.streams.filter(type="audio")
-    yt.first().download()
+    yt = yt.streams.filter(only_audio=True)
+    yt.first().download("./Downloads/Audios")
